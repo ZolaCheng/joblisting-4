472f7cc90b7713112424ef5e89819fb2e664014c
Zola Chen <zolacheng@gmail.com>
2017-06-29 11:00:12 +0800

    user can login/logout/signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require/alert
+//= require/dropdown
 //= require_tree .
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "新超人类！"
+  end
 end
