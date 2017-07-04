2013eea955a22104512414c1e94c2bbbcd831aef
Zola Chen <zolacheng@gmail.com>
2017-07-04 12:01:11 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def show
-    @job = Jobs.find(params[:id])
+    @job = Job.find(params[:id])
 
     if @job.is_hidden
       flash[:warning] = "This Job already archived"
---
+++ db/migrate/20170704001514_create_resumes.rb
@@ -0,0 +1,13 @@
+class CreateResumes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :resumes do |t|
+      t.integer :job_id
+      t.integer :user_id
+      t.text :content
+
+      t.string :attachment
+      t.timestamps
+
+    end
+  end
+end
